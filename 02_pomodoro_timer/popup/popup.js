41f70ac5bc423ea318f837e6a9c5d159b88374bf
sohail019 <shaikhhsohail0193@gmail.com>
2024-12-05 14:17:12 +0530

    Completed Pomodoro Timer

---
+++ 02_pomodoro_timer/popup/popup.js
@@ -0,0 +1,101 @@
+const startTimerBtn = document.getElementById("start-timer-btn");
+const addTaskBtn = document.getElementById("add-task-btn");
+
+function updateTime() {
+    chrome.storage.local.get(["timer", "timeOption"], (res) => {
+
+        const time = document.getElementById("time")
+        const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2, "0")
+        let seconds = "00"
+        if(res.timer % 60 != 0) {
+            seconds = `${60 - res.timer % 60}`.padStart(2, "0")
+        }
+       time.textContent = `${minutes}:${seconds}` 
+    } )
+}
+
+updateTime()
+setInterval(updateTime, 1000)
+let tasks = []
+
+startTimerBtn.addEventListener("click", () => {
+    chrome.storage.local.get("isRunning", (res) => {
+        chrome.storage.local.set({
+            isRunning: !res.isRunning
+        }, () => {
+            startTimerBtn.textContent = !res.Running ? "Pause Timer" : "Start Timer"
+        })
+    })
+})
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+    chrome.storage.local.set({
+        timer: 0,
+        isRunning: false
+    }, () => {
+        startTimerBtn.textContent = "Start Timer"
+    } )
+})
+
+addTaskBtn.addEventListener("click", () => addTask())
+
+
+chrome.storage.sync.get(["tasks"], (res) => {
+    tasks = res.tasks ? res.tasks  : []
+    renderTasks()
+})
+function saveTasks() {
+    chrome.storage.sync.set({
+        tasks,
+    })
+}
+
+function renderTask (taskNum) {
+const taskRow = document.createElement("div");
+
+const text = document.createElement("input");
+text.type = "text";
+text.placeholder = "Enter a task...";
+text.className = "task-input";
+text.value = tasks[taskNum]
+text.addEventListener("change", () => {
+  tasks[taskNum] = text.value;
+  saveTasks()
+});
+
+const deleteBtn = document.createElement("input");
+deleteBtn.type = "button";
+deleteBtn.value = "X";
+deleteBtn.className = "task-delete";
+deleteBtn.addEventListener("click", () => {
+  deleteTask(taskNum)
+});
+
+taskRow.appendChild(text);
+taskRow.appendChild(deleteBtn);
+
+const taskContainer = document.getElementById("task-container");
+taskContainer.appendChild(taskRow);
+}
+
+function addTask  ()  {
+    const taskNum = tasks.length
+    tasks.push("")
+    renderTask(taskNum)
+    saveTasks()
+}
+
+function deleteTask (taskNum) {
+    tasks.splice(taskNum, 1);
+    renderTasks()
+    saveTasks()
+}
+
+function renderTasks(){
+    const taskContainer = document.getElementById("task-container")
+    taskContainer.textContent = ""
+    tasks.forEach((taskText, taskNum) => {
+        renderTask(taskNum)
+    })
+}
